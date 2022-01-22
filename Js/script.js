369c3a1276667dbc330035a82c4ff124fa24c6a7
dtoxy <nikita12764586@gmail.com>
2022-01-22 10:20:25 +0200

    java

---
+++ Js/script.js
@@ -0,0 +1,275 @@
+// Дождь
+// let opady = prompt ('Будет ли сегодня дождь?')
+//  if (opady == true) {
+//      console.log ('Бери зонтик')
+//  }
+//  else {
+//      console.log('Дождя не будет')
+//  }
+
+// Задача №1
+// let x = prompt (`Ведите число`)
+
+// if (x>0){
+//     x = +x + +1
+//     console.log(x)
+// }
+// else{
+//     console.log(x)
+// }
+
+
+//Задача №2
+
+// let x = prompt (`Ведите число`)
+//  if (x>0){
+//      x = +x + +1
+//     console.log(x)
+//  }
+//  else{
+//      x = +x - +2
+//      console.log(x)
+//  }
+
+//Задача №3
+
+    // let x = prompt (`Ведите число`)
+    // if(x>0){
+    //     x = +x + +1
+    //     console.log(x)
+    // }
+
+    // if(x<0){
+    //     x = +x - +2
+    //     console.log(x)
+    // }
+
+    // else{
+    //     x = +x + +10
+    //     console.log(x)
+    // }
+
+//Задача №4
+
+// let x = prompt (`Ведите число №1`)
+// let y = prompt (`Ведите число №2`)
+// let z = prompt (`Ведите число №3`)
+
+//     if(x>0){
+//         x = +x / +x
+//     }
+//     if(x<0){
+//         x = +x * 0
+//     }
+//     if(y>0){
+//         y = +y / +y
+//     }
+//     if(y<0){
+//         y = +y * 0
+//     }
+//     if(z>0){
+//         z = +z / +z
+//     }
+//     if(z<0){
+//         z = +z * 0
+//     }
+//     if(+x + +y + +z>0){
+//         n = +x + +y + +z
+//         console.log(n)
+//     }
+//     else{
+//         n =  0
+//         console.log(n)
+//     }
+
+//Задача №5
+
+    // let x = prompt (`Ведите число №1`)
+    // let y = prompt (`Ведите число №2`)
+    // let z = prompt (`Ведите число №3`)
+    // let m = 1
+    // if(x>0){
+    //     x = +x / +x 
+    // }
+    // if(x<0){
+    //     x = +x * 0
+    // }
+    // if(y>0){
+    //     y = +y / +y 
+    // }
+    // if(y<0){
+    //     y = +y *0
+    // }
+    // if(z>0){
+    //     z = +z / +z 
+    // }
+    // if(z<0){
+    //     z = +z * 0
+    // }
+    // if(m>0){
+    //     n = +x + +y + +z
+    //     v = 3 - (+x + +y + +z)
+    //     console.log( "Количество положительних:" + "   " + n)
+    //     console.log( "Количество отрецательных:" + "   " + v)
+    // }
+
+// Задача №6
+
+// let x = prompt (`Ведите число №1`)
+// let y = prompt (`Ведите число №2`)
+
+// if(x>y){
+//     console.log(x)
+// }
+
+// else{
+//     console.log(y)
+// }
+
+// Задача №7
+
+// let x = prompt (`Ведите число №1`)
+// let y = prompt (`Ведите число №2`)
+// let m = 1
+// if (m>0){
+//     a = +x / +x
+//     b = +y / +y
+// }
+// if(x>y){
+//     g = +a + 1
+//     v = +g - +b
+//     console.log("№"+ "  "+v)
+// }
+
+// if(x<y){
+//     g = +a + 2
+//     v= +g - +b
+//     console.log("№"+ "  "+v)
+// }
+// if(x==y){
+//     console.log("Одинаковые")
+// }
+
+// Задача №8
+
+// let x = prompt (`Ведите число №1`)
+// let y = prompt (`Ведите число №2`)
+// let m = 1
+// if (m>0){
+//     a = +x / +x
+//     b = +y / +y
+// }
+// if(x>y){
+//     o = +a + 1
+//     g = +a - 1
+// }
+// if(y>x){
+//     g = +b + 1
+//     o = +a - 1
+// }
+// if(o>1){
+//     console.log(x)
+//     console.log(y)
+// }
+// if(g>1){
+//     console.log(x)
+//     console.log(y)
+// }
+
+// if(x==y){
+//     console.log("Одинаковые")
+// }
+
+// Задача №9
+
+// let x = prompt (`Ведите число №1`)
+// let y = prompt (`Ведите число №2`)
+// let m = 1
+
+// if(m>0){
+//     a= +x / +x
+//     b= +y / +y
+// }
+// if(x>y){
+//     a= +a + 3
+// }
+// if(a>2){
+//     console.log(`A =` + ` ` + y)
+//     console.log(`B =` + ` ` + x)
+// }
+// if(a<2){
+//     console.log(`A =` + ` ` + x)
+//     console.log(`B =` + ` ` + y)
+// }
+
+// Задача №10
+
+// let x = prompt (`Ведите число №1`)
+// let y = prompt (`Ведите число №2`)
+// let m = 1
+
+// if(m>0){
+//     a= +x / +x
+//     b= +y / +y
+// }
+// if(x !== y){
+//     a= +x + +y
+//     b= +x + +y
+//     console.log(`A =` + ` ` + a)
+//     console.log(`B =` + ` ` + a)
+// }
+// if(x == y){
+//     a= (+x + +y) * 0
+//     b= (+x + +y) * 0
+//     console.log(`A =` + ` ` + a)
+//     console.log(`B =` + ` ` + a)
+// }
+
+// Задача №11
+
+// let x = prompt (`Ведите число №1`)
+// let y = prompt (`Ведите число №2`)
+// let m = 1
+
+// if(m>0){
+    // a= +x / +x
+    // b= +y / +y
+// }
+// if(x !== y){
+//     if(x>y){
+//         a= +a + 3
+//     }
+//     if(a>2){
+//         console.log(`A =` + ` ` + x)
+//         console.log(`B =` + ` ` + x)
+//     }
+//     if(a<2){
+//         console.log(`A =` + ` ` + y)
+        // console.log(`B =` + ` ` + y)
+//     }
+// }
+// if(x == y){
+//     a= (+x + +y) * 0
+//     b= (+x + +y) * 0
+//     console.log(`A =` + ` ` + a)
+    // console.log(`B =` + ` ` + a)
+// }
+
+// Задача №12
+
+let x = prompt (`Ведите число №1`)
+let y = prompt (`Ведите число №2`)
+let z = prompt (`Ведите число №3`)
+let m = 1
+
+if(m>1){
+    a= +x / +x
+    b= +y / +y
+    d= +z / +z
+}
+
+if(x>y){
+    a= +a + 2
+    b= +b - 2
+    d= + - 2
+}
